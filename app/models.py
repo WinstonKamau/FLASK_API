051eecdddcf0e549ebec7463f0ca1faa7424f86f
Winston <winstonkamau77@gmail.com>
2017-08-12 21:59:12 +0300

    Created methods for Activities

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 
 class BucketList(db.Model):
-    '''The table for the bucketlist with the variable name __buckettable__'''
+    #The table for the bucketlist with the variable name __buckettable__
     __buckettable__ = 'bucketlists'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -34,4 +34,35 @@ class BucketList(db.Model):
     def __repr__(self):
         '''A method that repesents the object instance of the model whenever it queries'''
         return "Bucketlist: {}>".format(self.name)
-        
+
+
+class Activities(db.Model):
+   #table for the activities with the activity name activities
+    __activitylist__="activities"
+    id = db.Column(db.Integer, primary_key=True)
+    activity_name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default = db.func.current_timestamp(), 
+                              onupdate = db.func.current_timestamp())
+
+    def __init__(self, activity_name):
+        '''initialising the activity name'''
+        self.activity_name = activity_name
+    
+    def save_activity(self):
+        '''A method to save the activity name'''
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_activity(self):
+        '''A method to delete the activity name'''
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def read_activity():
+        return Activities.query.all()
+    
+    def __repr__(self):
+        '''A method that returns an object instance of Activity whenever it queries'''
+        return "Activities: {}>".format(self.name)
