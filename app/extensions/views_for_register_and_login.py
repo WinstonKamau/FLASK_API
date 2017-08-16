55d60851fd686875a6ceda5ef9fe2483df29e4dc
Winston <winstonkamau77@gmail.com>
2017-08-16 18:32:41 +0300

    Refined tests according to register and login

--- app/extensions/views_for_register_and_login.py
+++ app/extensions/views_for_register_and_login.py
@@ -1,5 +1,5 @@
 from . import registration_login_blueprint
-
+import re
 from flask.views import MethodView
 from flask import make_response, request, jsonify
 from app.models import User
@@ -8,60 +8,68 @@ from app.models import User
 class Registration(MethodView):
 
     def post(self):
-        user = User.query.filter_by(user_email=request.data['user_email']).first()
-
-        if not user:
-            try:
+        if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', request.data['user_email']):
+            user = User.query.filter_by(user_email=request.data['user_email']).first()
+            if not user:
+                
                 data_posted = request.data
                 user_email = data_posted['user_email']
-                user_password = data_posted['user_password']
-                user = User(user_email=user_email, user_password=user_password)
-                user.save_user()
+                try:
+                    user_password = data_posted['user_password']
+                    print(user_email)
+                    print(user_password)
+                    user = User(user_email=user_email, user_password=user_password)
+                    user.save_user()
+                    response = {
+                        'message': 'Email and password entered correctly. Use them to log in'
+                    }
+                    return make_response(jsonify(response)), 201
+                except Exception as e:
+                    response = {
+                        'message': str(e)
+                    }
+                    return make_response(jsonify(response)), 401
+            else:
                 response = {
-                    'message': 'Email and password entered correctly. Use them to log in'
+                    'message':'The user email entered already exists!'
                 }
-                return make_response(jsonify(response)), 201
-            except Exception as e:
-                response = {
-                    'message': str(e)
-                }
-                return make_response(jsonify(response)), 401
-        
+
+                return make_response(jsonify(response)), 202
         else:
             response = {
-                'message':'The user email entered already exists. '
+                'message': 'The user email entered is not valid ensure it has @ and .'
             }
-
-            return make_response(jsonify(response)), 202
-
+            return make_response(jsonify(response)), 400
 
 class Login(MethodView):
     def post(self):
-        user = User.query.filter_by(user_email=request.data['user_email']).first()
-        post_data = request.data
-        if user:
-            if user.password_confirm(post_data['user_password']):
-                token = user.create_encoded_token(user.id)
-                response = {
-                    'message': 'User logged in',
-                    'access-token': user.decode_token_to_sub(token) 
-                }
-                return make_response(jsonify(response)), 200
+        if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', request.data['user_email']):
+            user = User.query.filter_by(user_email=request.data['user_email']).first()
+            post_data = request.data
+            if user:
+                if user.password_confirm(post_data['user_password']):
+                    token = user.create_encoded_token(user.id)
+                    if token:
+                        response = {
+                            'message': 'User logged in',
+                            'access-token': token.decode()
+                        }
+                        return make_response(jsonify(response)), 200
+                else:
+                    response = {
+                        'message': 'Wrong password entered for the user %s !'%(request.data['user_email'])
+                    }
+                    return make_response(jsonify(response)), 401   
             else:
                 response = {
-                    'message': 'Wrong password entered'
+                    'message': 'Username does not exist! Register or check the username entered again.'
                 }
-                return make_response(jsonify(response)), 401   
+                return make_response(jsonify(response)), 401
         else:
             response = {
-                'message': 'Wrong user name entered'
+                'message': 'The user email entered is not valid ensure it has @ and .'
             }
-            return make_response(jsonify(response)), 401
-
-
-
-
-
+            return make_response(jsonify(response)), 400
 
 
 view_for_registration = Registration.as_view('register_view')
