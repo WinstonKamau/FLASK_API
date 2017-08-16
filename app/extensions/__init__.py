5e2c45b000c7c97d76a368ccd465d10749e8c0b5
Winston <winstonkamau77@gmail.com>
2017-08-16 06:28:52 +0300

    Refactoring based on tests done

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-registration_login_blueprint = Blueprint('author', __name__)
+registration_login_blueprint = Blueprint('auth', __name__)
 
 from . import views_for_register_and_login
