fe0e9711a9fba8d37f7efa344d8360a341fba104
Winston <winstonkamau77@gmail.com>
2017-08-17 21:25:06 +0300

    Refactored test to consider tokenization

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -1,6 +1,5 @@
 '''A module to test the Bucket List class on crud for bucketlists '''
 import unittest
-import os
 import json
 from app import create_app, db
 
@@ -13,64 +12,97 @@ class TheBucketTestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
         self.bucketlist = {"name": "Climb the Himalayas"}
-
+        self.user_information = {
+            "user_email": "user1@gmail.com",
+            "user_password": "password"
+            }
         with self.app.app_context():
             db.create_all()
 
+    def register_login_and_return_token(self):
+        '''A method that registers a user, logs in a user, creates a token
+        for the user and returns it'''
+        self.client().post('/auth/register', data=self.user_information)
+        result_of_login = self.client().post('/auth/login', data=self.user_information)
+        token = json.loads(result_of_login.data.decode())['access-token']
+        return token
+
+    def post_a_bucket(self):
+        '''A method that posts a bucket to the test_db, ideally all other
+        methods use it so it acts as a method for posting for tests below.
+        The method posts after registering and logging in
+        '''
+        return self.client().post('/bucketlists/',
+                                  headers=dict(Authorization='Bearer '
+                                               + self.register_login_and_return_token()
+                                              ),
+                                  data=self.bucketlist
+                                 )
+
     def test_bucketlist_creation(self):
         """A method to test that the API creates a bucket"""
-        result_of_post_method = self.client().post('/bucketlists/', data=self.bucketlist)
-        self.assertEqual(result_of_post_method.status_code, 201)
-        self.assertIn('Climb the Himalayas', str(result_of_post_method.data))
+        post_data = self.post_a_bucket()
+        self.assertEqual(post_data.status_code, 201)
+        self.assertIn('Climb the Himalayas', str(post_data.data))
 
     def test_read_bucket(self):
         """A method to test that the api reads a bucket"""
-        result_of_post_method = self.client().post('/bucketlists/', data=self.bucketlist)
-        self.assertEqual(result_of_post_method.status_code, 201)
-        result_of_get_method = self.client().get('/bucketlists/')
+        post_data = self.post_a_bucket()
+        self.assertEqual(post_data.status_code, 201)
+        result_of_get_method = self.client().get('/bucketlists/',
+                                                 headers=dict(Authorization='Bearer '+ self.register_login_and_return_token())
+                                                )
         self.assertEqual(result_of_get_method.status_code, 200)
         self.assertIn('Climb the Himalayas', str(result_of_get_method.data))
 
 
     def test_read_bucket_using_id(self):
-        """A method to test the retrieval of one bucket using an id placed in brackets
+        '''A method to test the retrieval of one bucket using an id placed in brackets
         on the after the '/bucketlists/' statement below
-        """
-        #a method to post the dictionary "name": "Climb the Himalayas"
-        result_of_post_method = self.client().post('/bucketlists/', data=self.bucketlist)
-        #a method to test that the post worked
-        self.assertEqual(result_of_post_method.status_code, 201)
+        '''
+        post_data = self.post_a_bucket()
+        self.assertEqual(post_data.status_code, 201)
         #converting the response from posting to json format
-        json_data = json.loads(result_of_post_method.data.decode('utf-8').replace("'", "\""))
-        final_data = self.client().get(
-            '/bucketlists/{}'.format(json_data['id']))
+        json_data = json.loads(post_data.data.decode('utf-8').replace("'", "\""))
+        final_data = self.client().get('/bucketlists/{}'.format(json_data['id']),
+                                       headers=dict(Authorization='Bearer '
+                                                    + self.register_login_and_return_token())
+                                      )
         self.assertEqual(final_data.status_code, 200)
         self.assertIn('Climb the Himalayas', str(final_data.data))
 
     def test_edit_bucketlist(self):
         """A method to test the editing of a bucket list"""
-        result_of_post_method = self.client().post(
-            '/bucketlists/',
-            data={'name': 'Summer, Winter'})
-        self.assertEqual(result_of_post_method.status_code, 201)
+        post_data = self.post_a_bucket()
+        self.assertEqual(post_data.status_code, 201)
         result_of_put_method = self.client().put(
             '/bucketlists/1',
+            headers=dict(Authorization='Bearer '
+                         + self.register_login_and_return_token()
+                        ),
             data={
                 "name": "The seasons will be, summer winter and autumn"
             })
         self.assertEqual(result_of_put_method.status_code, 200)
-        result_of_get_method = self.client().get('/bucketlists/1')
+        result_of_get_method = self.client().get('/bucketlists/1',
+                                                 headers=dict(Authorization='Bearer '
+                                                              + self.register_login_and_return_token())
+                                                )
         self.assertIn('The seasons will b', str(result_of_get_method.data))
 
     def test_delete_bucketlist(self):
         """A method to test the deleting of a bucket list"""
-        result_of_post_method = self.client().post(
-            '/bucketlists/',
-            data={'name': 'Summer , Winter'})
-        self.assertEqual(result_of_post_method.status_code, 201)
-        result_of_delete_method = self.client().delete('/bucketlists/1')
+        post_data = self.post_a_bucket()
+        self.assertEqual(post_data.status_code, 201)
+        result_of_delete_method = self.client().delete('/bucketlists/1',
+                                                       headers=dict(Authorization='Bearer '
+                                                                    + self.register_login_and_return_token())
+                                                      )
         self.assertEqual(result_of_delete_method.status_code, 200)
-        response_after_removal = self.client().get('/bucketlists/1')
+        response_after_removal = self.client().get('/bucketlists/1',
+                                                   headers=dict(Authorization='Bearer '
+                                                                + self.register_login_and_return_token())
+                                                  )
         self.assertEqual(response_after_removal.status_code, 404)
 
     def tearDown(self):
