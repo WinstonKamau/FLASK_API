3291946d471ca05e7de1cee6b936a45ab06d77c0
Winston <winstonkamau77@gmail.com>
2017-08-20 22:20:09 +0300

    Refactored code to allow reseting of passwords

--- app/extensions/views_for_register_and_login.py
+++ app/extensions/views_for_register_and_login.py
@@ -3,6 +3,7 @@ import re
 from flask.views import MethodView
 from flask import make_response, request, jsonify
 from app.models import User
+from flask_bcrypt import Bcrypt
 
 
 class Registration(MethodView):
@@ -11,7 +12,6 @@ class Registration(MethodView):
         if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', request.data['user_email']):
             user = User.query.filter_by(user_email=request.data['user_email']).first()
             if not user:
-                
                 data_posted = request.data
                 user_email = data_posted['user_email']
                 try:
@@ -70,6 +70,45 @@ class Login(MethodView):
             return make_response(jsonify(response)), 400
 
 
+class ResetPassword(MethodView):
+    def post(self):
+        header = request.headers.get('Authorization')
+        splitted_header = header.split(' ')
+        token = splitted_header[1]
+        user_id = User.decode_token_to_sub(token)
+        if not isinstance(user_id, str):
+            current_password = request.data.get('user_password')
+            user = User.query.filter_by(id=user_id).first()
+            if user.password_confirm(current_password):
+                new_password = request.data.get('new_password')
+                verify_new_password = request.data.get ('verify_new_password')
+                if new_password == verify_new_password:
+                    user.user_password = Bcrypt().generate_password_hash(new_password).decode()
+                    user.save_user()
+                    response = jsonify({
+                        'user_email': user.user_email,
+                        'user_password': user.user_password,
+                        'message': 'Password reset'
+                    })
+                    return make_response (response), 201
+                else:
+                    response = jsonify({
+                        'message': 'Passwords do not match'
+                    })
+                    return make_response(response), 400
+                
+            else:
+                response = jsonify({
+                    'message': 'Wrong password entered'
+                })
+                return make_response(response), 400
+        else:
+            response = jsonify({
+                'message': 'Invalid token entered'
+            })
+            return make_response(response), 400
+
+
 view_for_registration = Registration.as_view('register_view')
 
 registration_login_blueprint.add_url_rule(
@@ -82,5 +121,13 @@ view_for_login = Login.as_view('login_view')
 registration_login_blueprint.add_url_rule(
     '/auth/login',
     view_func=view_for_login,
+    methods=['POST']
+)
+
+view_for_reset_password = ResetPassword.as_view('reset_password_view')
+
+registration_login_blueprint.add_url_rule(
+    '/auth/reset-password',
+    view_func=view_for_reset_password,
     methods=['POST']
 )
