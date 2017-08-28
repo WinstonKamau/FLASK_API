bbaf0f5486f939cab498b145a5ed236c93936370
Winston <winstonkamau77@gmail.com>
2017-08-28 09:53:28 +0300

    Refactoring code to reading standards

--- tests/test_registration_login_token.py
+++ tests/test_registration_login_token.py
@@ -1,3 +1,6 @@
+'''A module for testing registration, login, reset password and
+logout
+'''
 import unittest
 import json
 from app import create_app
@@ -5,10 +8,13 @@ from app import db
 
 
 class RegistrationLoginTokenTestCase(unittest.TestCase):
-
+    '''A class for that instantiates a client for testing and has functions
+    for testing that the registration, login logout and reseting password work
+    '''
 
     def setUp(self):
-        self.app = create_app(config_name = 'testing')
+        '''A method to set up an instance of a client to test'''
+        self.app = create_app(config_name='testing')
         self.client = self.app.test_client
         self.information = {
             'user_email': 'user1@gmail.com',
@@ -20,42 +26,57 @@ class RegistrationLoginTokenTestCase(unittest.TestCase):
             db.drop_all()
             db.create_all()
 
-    def test_the_register_endpoint_function(self):
+    def register_one_user(self):
+        '''A method that registers one user'''
         result_of_post_method = self.client().post('/auth/register', data=self.information)
-        result = json.loads(result_of_post_method.data.decode())
+        return result_of_post_method
+
+    def test_the_register_endpoint_function(self):
+        '''A happy path to test registration with right information'''
+        post_result = self.register_one_user()
+        result = json.loads(post_result.data.decode())
         self.assertIn("Email and password entered correctly", result['message'])
-        self.assertEqual(result_of_post_method.status_code, 201)
+        self.assertEqual(post_result.status_code, 201)
 
     def test_null_registration_of_users_with_same_info(self):
-        result_of_post_method = self.client().post('/auth/register', data=self.information)
-        self.assertEqual(result_of_post_method.status_code, 201)
-        result_of_post_method_2 = self.client().post('/auth/register', data=self.information)
-        result = json.loads(result_of_post_method_2.data.decode())
+        '''A method to test no registration when the user had been registered
+        before
+        '''
+        post_result = self.register_one_user()
+        self.assertEqual(post_result.status_code, 201)
+        second_similar_post = self.register_one_user()
+        result = json.loads(second_similar_post.data.decode())
         self.assertEqual(result['message'], 'The user email entered already exists!')
-        self.assertEqual(result_of_post_method_2.status_code, 409)
+        self.assertEqual(second_similar_post.status_code, 409)
 
     def test_the_user_login(self):
-        result_of_post_method = self.client().post('/auth/register', data=self.information)
-        self.assertEqual(result_of_post_method.status_code, 201)
+        '''A method to thest that login works happy path and returns right variables'''
+        post_result = self.register_one_user()
+        self.assertEqual(post_result.status_code, 201)
         result_of_post_method = self.client().post('/auth/login', data=self.information)
         result = json.loads(result_of_post_method.data.decode())
         self.assertEqual(result['message'], 'User logged in')
-        self.assertNotEqual(result['access-token'], None )
+        self.assertNotEqual(result['access-token'], None)
         self.assertEqual(result_of_post_method.status_code, 200)
 
     def test_non_registered_user_login_with_wrong_user_email(self):
+        '''A method to test that a user cannot login with a wrong email'''
         user_data = {
             'user_email':'unknown_email@gmail.com',
             'user_password':'password'
         }
         result_of_post_method = self.client().post('/auth/login', data=user_data)
         result = json.loads(result_of_post_method.data.decode())
-        self.assertEqual(result['message'], 'User email does not exist! Register or check the user email entered again.')
+        self.assertEqual(result['message'],
+                         'User email does not exist! Register or check the user email entered again.')
         self.assertEqual(result_of_post_method.status_code, 401)
 
     def test_registered_user_with_wrong_password(self):
-        result_of_post_method = self.client().post('/auth/register', data=self.information)
-        self.assertEqual(result_of_post_method.status_code, 201)
+        '''A method to test that a user cannot login by inserting a wrong password
+        to his email address that had been registered
+        '''
+        post_result = self.register_one_user()
+        self.assertEqual(post_result.status_code, 201)
         user_data = {
             'user_email':'user1@gmail.com',
             'user_password': 'wrong_password'
@@ -66,8 +87,11 @@ class RegistrationLoginTokenTestCase(unittest.TestCase):
         self.assertEqual(result_of_post_method.status_code, 401)
 
     def test_reset_password(self):
-        result_of_post_method = self.client().post('/auth/register', data=self.information)
-        self.assertEqual(result_of_post_method.status_code, 201)
+        '''A method to test that the password is reset when all values inserted are
+        right
+        '''
+        post_result = self.register_one_user()
+        self.assertEqual(post_result.status_code, 201)
         result_of_login = self.client().post('/auth/login', data=self.information)
         self.assertEqual(result_of_login.status_code, 200)
         token = json.loads(result_of_login.data.decode())['access-token']
@@ -75,18 +99,21 @@ class RegistrationLoginTokenTestCase(unittest.TestCase):
             'user_password':'password',
             'new_password':'new_test_password',
             'verify_new_password': 'new_test_password'
-        } 
+        }
         result_of_post_method = self.client().post('auth/reset-password',
                                                   headers=dict(Authorization='Bearer '+ token),
                                                   data=reset_data
                                                   )
-        
+
         self.assertEqual(result_of_post_method.status_code, 201)
         self.assertIn('Password reset', str(result_of_post_method.data))
 
     def test_reset_password_sad_path(self):
-        result_of_post_method = self.client().post('/auth/register', data=self.information)
-        self.assertEqual(result_of_post_method.status_code, 201)
+        '''A method to test that the reset password does not work when the
+        wrong password has been used
+        '''
+        post_result = self.register_one_user()
+        self.assertEqual(post_result.status_code, 201)
         result_of_login = self.client().post('/auth/login', data=self.information)
         self.assertEqual(result_of_login.status_code, 200)
         token = json.loads(result_of_login.data.decode())['access-token']
@@ -94,18 +121,20 @@ class RegistrationLoginTokenTestCase(unittest.TestCase):
             'user_password':'wrongpassword',
             'new_password':'new_test_password',
             'verify_new_password': 'new_test_password'
-        } 
+        }
         result_of_post_method = self.client().post('auth/reset-password',
                                                   headers=dict(Authorization='Bearer '+ token),
                                                   data=reset_data
                                                   )
-        
         self.assertEqual(result_of_post_method.status_code, 400)
         self.assertIn('Wrong password entered', str(result_of_post_method.data))
 
     def test_reset_password_sad_path_1(self):
-        result_of_post_method = self.client().post('/auth/register', data=self.information)
-        self.assertEqual(result_of_post_method.status_code, 201)
+        '''A method to test that reset password cannot work when the new password
+        does not match the verify new password
+        '''
+        post_result = self.register_one_user()
+        self.assertEqual(post_result.status_code, 201)
         result_of_login = self.client().post('/auth/login', data=self.information)
         self.assertEqual(result_of_login.status_code, 200)
         token = json.loads(result_of_login.data.decode())['access-token']
@@ -120,4 +149,5 @@ class RegistrationLoginTokenTestCase(unittest.TestCase):
                                                   )
         
         self.assertEqual(result_of_post_method.status_code, 400)
-        self.assertIn('New password and verify new password do not match', str(result_of_post_method.data))
+        self.assertIn('New password and verify new password do not match',
+                       str(result_of_post_method.data))
