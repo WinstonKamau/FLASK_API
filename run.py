fc58f9a6ca379ba4ca5b2e9b72fdde32293cf3a5
WinstonKamau <crispus.kamau@andela.com>
2018-02-02 17:22:04 +0300

    chore(cors)

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 '''Module for running the bucketlist flask api'''
 import os
 from app import create_app
+from flask_cors import CORS
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
+CORS(app)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
